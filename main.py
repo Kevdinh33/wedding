e34cfe11cee4e1c81ae169c03cda9f97d3676ec0
KD <kevdinh33@gmail.com>
2023-11-09 22:46:43 -0600

    first draft

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
-def hello():
+def home():
     return render_template ("index.html")
-
-@app.route("/about")
-def about():
-    return render_template ("about.html")
-
-@app.route("/ceremony")
-def ceremony():
-    return render_template ("ceremony.html")
-
-@app.route("/reception")
-def reception():
-    return render_template ("reception.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
