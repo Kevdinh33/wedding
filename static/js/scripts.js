b5e1eb41282bf189391c8f81af8069cf317b3888
kevdinh33 <kevdinh33@gmail.com>
2024-03-08 15:03:36 -0600

    schedule

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -57,3 +57,55 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+function openImagePopup() {
+    var imagePopup = document.createElement("div");
+    imagePopup.id = "imagePopup";
+  
+    // Popup styles
+    imagePopup.style.position = "fixed";
+    imagePopup.style.top = "50px";
+    imagePopup.style.left = "0";
+    imagePopup.style.width = "100%";
+    imagePopup.style.height = "100%";
+    imagePopup.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    imagePopup.style.zIndex = "100";
+  
+    // Create and style the close button (optional)
+    var closeButton = document.createElement("button");
+    closeButton.textContent = "X";
+    closeButton.style.position = "absolute";
+    closeButton.style.top = "10px";
+    closeButton.style.right = "10px";
+    closeButton.style.fontSize = "30px";
+    closeButton.style.backgroundColor = "transparent";
+    closeButton.style.border = "none";
+    closeButton.style.cursor = "pointer";
+  
+    // Add click event listener to close button (optional)
+    closeButton.addEventListener("click", function() {
+      document.body.removeChild(imagePopup);
+    });
+  
+    // Create and style the image
+    var image = document.createElement("img");
+    image.src = "/static/assets/img/portfolio/WeddingSchedule.jpg";
+    image.style.width = "auto";
+    image.style.height = "100%";
+    image.style.display = "block";
+    image.style.margin = "auto";
+  
+    // Click outside image to close popup
+    imagePopup.addEventListener("click", function(event) {
+      if (event.target !== image) {
+        document.body.removeChild(imagePopup);
+      }
+    });
+  
+    // Append elements and display popup
+    if (closeButton) { // Add close button if it exists
+      imagePopup.appendChild(closeButton);
+    }
+    imagePopup.appendChild(image);
+    document.body.appendChild(imagePopup);
+  }
+openImagePopup()
